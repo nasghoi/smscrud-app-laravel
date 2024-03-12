82b9cb9b24c2301e78bd5a6141fe1c3215c3b478
Nasrullah Jam Sarim <muhdnas10@gmail.com>
2024-03-12 17:59:17 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::resource('/students', StudentController::class);
+Route::resource('/', StudentController::class);
